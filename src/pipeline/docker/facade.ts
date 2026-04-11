9f0e187075b3ab4fe62c08dd1c253a369329d2c2
lehdqlsl <lehdqlsl@naver.com>
2026-04-11 17:11:43 +0900

    refactor(pipeline): replace docker monolith with facade + barrel re-export
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ src/pipeline/docker/facade.ts
@@ -0,0 +1,231 @@
+import { createDockerContext, type DockerContext } from './context.js';
+import { ContainerOps } from './container.js';
+import { ExecOps } from './exec.js';
+import { cleanupSecretFiles, dockerStatus } from './helpers.js';
+import { ImageOps } from './image.js';
+import { InfraOps } from './infra.js';
+import { NetworkOps } from './network.js';
+import { StreamOps } from './stream.js';
+import type { DockerStatus } from './types.js';
+import { VolumeOps } from './volume.js';
+
+export class Docker {
+  private readonly ctx: DockerContext;
+  private readonly containerOps: ContainerOps;
+  private readonly imageOps: ImageOps;
+  private readonly networkOps: NetworkOps;
+  private readonly volumeOps: VolumeOps;
+  private readonly execOps: ExecOps;
+  private readonly streamOps: StreamOps;
+  private readonly infraOps: InfraOps;
+
+  constructor(socketPath?: string, networkName?: string) {
+    this.ctx = createDockerContext(socketPath, networkName);
+    this.networkOps = new NetworkOps(this.ctx);
+    this.containerOps = new ContainerOps(this.ctx, {
+      ensureSharedNetworkAttachment: (containerId, alias) =>
+        this.networkOps.ensureSharedNetworkAttachment(containerId, alias),
+      connectToNetworkStrict: (containerId, strictNetworkName) =>
+        this.networkOps.connectToNetworkStrict(containerId, strictNetworkName),
+    });
+    this.imageOps = new ImageOps(this.ctx);
+    this.volumeOps = new VolumeOps(this.ctx);
+    this.execOps = new ExecOps(this.ctx);
+    this.streamOps = new StreamOps(this.ctx);
+    this.infraOps = new InfraOps(this.ctx);
+  }
+
+  runContainer(...args: Parameters<ContainerOps['runContainer']>) {
+    return this.containerOps.runContainer(...args);
+  }
+
+  runComposeService(...args: Parameters<ContainerOps['runComposeService']>) {
+    return this.containerOps.runComposeService(...args);
+  }
+
+  runInfraContainer(...args: Parameters<ContainerOps['runInfraContainer']>) {
+    return this.containerOps.runInfraContainer(...args);
+  }
+
+  stopContainer(...args: Parameters<ContainerOps['stopContainer']>) {
+    return this.containerOps.stopContainer(...args);
+  }
+
+  startContainer(...args: Parameters<ContainerOps['startContainer']>) {
+    return this.containerOps.startContainer(...args);
+  }
+
+  removeContainer(...args: Parameters<ContainerOps['removeContainer']>) {
+    return this.containerOps.removeContainer(...args);
+  }
+
+  safeRemoveContainer(...args: Parameters<ContainerOps['safeRemoveContainer']>) {
+    return this.containerOps.safeRemoveContainer(...args);
+  }
+
+  inspectContainer(...args: Parameters<ContainerOps['inspectContainer']>) {
+    return this.containerOps.inspectContainer(...args);
+  }
+
+  restartContainer(...args: Parameters<ContainerOps['restartContainer']>) {
+    return this.containerOps.restartContainer(...args);
+  }
+
+  getContainerStats(...args: Parameters<ContainerOps['getContainerStats']>) {
+    return this.containerOps.getContainerStats(...args);
+  }
+
+  renameContainer(...args: Parameters<ContainerOps['renameContainer']>) {
+    return this.containerOps.renameContainer(...args);
+  }
+
+  waitForContainer(...args: Parameters<ContainerOps['waitForContainer']>) {
+    return this.containerOps.waitForContainer(...args);
+  }
+
+  runServiceContainer(...args: Parameters<ContainerOps['runServiceContainer']>) {
+    return this.containerOps.runServiceContainer(...args);
+  }
+
+  waitForHealthy(...args: Parameters<ContainerOps['waitForHealthy']>) {
+    return this.containerOps.waitForHealthy(...args);
+  }
+
+  listManagedContainers(...args: Parameters<ContainerOps['listManagedContainers']>) {
+    return this.containerOps.listManagedContainers(...args);
+  }
+
+  listAllContainers(...args: Parameters<ContainerOps['listAllContainers']>) {
+    return this.containerOps.listAllContainers(...args);
+  }
+
+  buildImage(...args: Parameters<ImageOps['buildImage']>) {
+    return this.imageOps.buildImage(...args);
+  }
+
+  cancelBuild(...args: Parameters<ImageOps['cancelBuild']>) {
+    return this.imageOps.cancelBuild(...args);
+  }
+
+  buildComposeService(...args: Parameters<ImageOps['buildComposeService']>) {
+    return this.imageOps.buildComposeService(...args);
+  }
+
+  pullImage(...args: Parameters<ImageOps['pullImage']>) {
+    return this.imageOps.pullImage(...args);
+  }
+
+  inspectImage(...args: Parameters<ImageOps['inspectImage']>) {
+    return this.imageOps.inspectImage(...args);
+  }
+
+  removeImage(...args: Parameters<ImageOps['removeImage']>) {
+    return this.imageOps.removeImage(...args);
+  }
+
+  tagImage(...args: Parameters<ImageOps['tagImage']>) {
+    return this.imageOps.tagImage(...args);
+  }
+
+  getImageExposedPort(...args: Parameters<ImageOps['getImageExposedPort']>) {
+    return this.imageOps.getImageExposedPort(...args);
+  }
+
+  listDanglingImages(...args: Parameters<ImageOps['listDanglingImages']>) {
+    return this.imageOps.listDanglingImages(...args);
+  }
+
+  ensureSharedNetworkAttachment(...args: Parameters<NetworkOps['ensureSharedNetworkAttachment']>) {
+    return this.networkOps.ensureSharedNetworkAttachment(...args);
+  }
+
+  connectContainerToNetwork(...args: Parameters<NetworkOps['connectContainerToNetwork']>) {
+    return this.networkOps.connectContainerToNetwork(...args);
+  }
+
+  disconnectContainerFromNetwork(
+    ...args: Parameters<NetworkOps['disconnectContainerFromNetwork']>
+  ) {
+    return this.networkOps.disconnectContainerFromNetwork(...args);
+  }
+
+  getNetworkInfo(...args: Parameters<NetworkOps['getNetworkInfo']>) {
+    return this.networkOps.getNetworkInfo(...args);
+  }
+
+  ensureProjectNetwork(...args: Parameters<NetworkOps['ensureProjectNetwork']>) {
+    return this.networkOps.ensureProjectNetwork(...args);
+  }
+
+  removeProjectNetwork(...args: Parameters<NetworkOps['removeProjectNetwork']>) {
+    return this.networkOps.removeProjectNetwork(...args);
+  }
+
+  ensureNetwork(...args: Parameters<NetworkOps['ensureNetwork']>) {
+    return this.networkOps.ensureNetwork(...args);
+  }
+
+  inspectVolume(...args: Parameters<VolumeOps['inspectVolume']>) {
+    return this.volumeOps.inspectVolume(...args);
+  }
+
+  listVolumes(...args: Parameters<VolumeOps['listVolumes']>) {
+    return this.volumeOps.listVolumes(...args);
+  }
+
+  createVolume(...args: Parameters<VolumeOps['createVolume']>) {
+    return this.volumeOps.createVolume(...args);
+  }
+
+  removeVolume(...args: Parameters<VolumeOps['removeVolume']>) {
+    return this.volumeOps.removeVolume(...args);
+  }
+
+  execSimple(...args: Parameters<ExecOps['execSimple']>) {
+    return this.execOps.execSimple(...args);
+  }
+
+  execStream(...args: Parameters<ExecOps['execStream']>) {
+    return this.execOps.execStream(...args);
+  }
+
+  execTerminal(...args: Parameters<ExecOps['execTerminal']>) {
+    return this.execOps.execTerminal(...args);
+  }
+
+  getLogs(...args: Parameters<StreamOps['getLogs']>) {
+    return this.streamOps.getLogs(...args);
+  }
+
+  getLogStream(...args: Parameters<StreamOps['getLogStream']>) {
+    return this.streamOps.getLogStream(...args);
+  }
+
+  getEventStream(...args: Parameters<StreamOps['getEventStream']>) {
+    return this.streamOps.getEventStream(...args);
+  }
+
+  ping(...args: Parameters<InfraOps['ping']>) {
+    return this.infraOps.ping(...args);
+  }
+
+  ensureRunning(...args: Parameters<InfraOps['ensureRunning']>) {
+    return this.infraOps.ensureRunning(...args);
+  }
+
+  getDiskUsage(...args: Parameters<InfraOps['getDiskUsage']>) {
+    return this.infraOps.getDiskUsage(...args);
+  }
+
+  async status(): Promise<DockerStatus> {
+    return dockerStatus(this.ctx.client);
+  }
+
+  cleanupSecretFiles(name: string): void {
+    cleanupSecretFiles(name);
+  }
+
+  getNetworkName(): string {
+    return this.ctx.networkName;
+  }
+}
